7d4bdafbb3bce3091c545640391b560b2ba1b3de
Sebastián Zamorano A <sebastian.zamorano@microsoft.com>
2024-02-27 07:07:52 -0500

    Pre-release please don´t use

--- MPARR2/ConfigFiles/MPARR-GetTrackingRMSData.ps1
+++ MPARR2/ConfigFiles/MPARR-GetTrackingRMSData.ps1
@@ -1,4 +1,52 @@
-﻿<#
+﻿<#PSScriptInfo
+
+.VERSION 2.0.4
+
+.GUID 883af802-165c-4700-b4c1-352686c02f01
+
+.AUTHOR 
+https://www.linkedin.com/in/grzegorzberdzik/; Grzegorz Berdzik
+https://www.linkedin.com/in/profesorkaz/; Sebastian Zamorano
+
+.COMPANYNAME 
+Microsoft Purview Advanced Rich Reports
+
+.TAGS 
+#Microsoft365 #M365 #MPARR #MicrosoftPurview #PowerBI #LogsAnalytics #Sentinel #Reporting #Dashboards #InformationProtection #MIP #Labels #DLP
+#Webinar #PowerBI #DataAnalisys #Data #DataInsights #API #Office365ManagementAPI #YouTube #DataExfiltration
+
+.PROJECTURI 
+https://aka.ms/MPARR-GitHub; https://aka.ms/MPARR-LinkedIn; https://aka.ms/MPARR-YouTube 
+
+.RELEASENOTES
+The MIT License (MIT)
+Copyright (c) 2015 Microsoft Corporation
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+#>
+
+<# 
+
+.DESCRIPTION 
+Script to collect data using RMS API for tracking purpose
+
+#>
+
+<#
 .NOTES
 Script to collect data using RMS API
 This script needs PowerShell 5 and is called from MPARR_RMSData2.ps1
